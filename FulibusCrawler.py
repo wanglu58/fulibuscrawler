879c08c8b1ebd8f7e0af3f89d3a1558154197086
wanglu58 <38058604+wanglu58@users.noreply.github.com>
2019-09-01 09:50:42 +0800

    Optimization

--- FulibusCrawler.py
+++ FulibusCrawler.py
@@ -5,9 +5,9 @@ import os
 import sys
 import threading
 
-# 定义爬取福利吧的函数
-def scrawler(issue_start,issue_end,page):
-    for i in range(issue_start, issue_end):
+# 定义爬取福利吧-福利汇总的函数
+def scrawler(start,end,page):
+    for i in range(start,end):
         try:
             response = requests.get('https://fulibus.net/%s.html/%s' % (str(i), page), timeout=(30, 30))
             path = os.path.abspath('.')
@@ -15,22 +15,20 @@ def scrawler(issue_start,issue_end,page):
             try:
                 os.makedirs(lujing)
             except FileExistsError as e:
-                print(str(e) + '请删除该目录,再重新运行.')
+                print(str(e) + '请删除该目录，再重新运行。')
                 sys.exit()
             soup = BeautifulSoup(response.content, 'lxml')
             data = soup.find_all('p')
             dnumber = 0
             inumber = 0
-            print('网页' + str(i) + '开始下载第%s页...'%page)
             for d in data:
                 dnumber += 1
                 imgs = d.find_all('img')
                 for img in imgs:
                     inumber += 1
                     tupian_url = img.get('src')
-                    print(tupian_url)
                     count = str(i) + '-' + page + '-' + str(dnumber) + '-' + str(inumber)
-                    print('正在下载' + count + '的图片')
+                    print('正在下载%s图片，链接：%s'%(count,tupian_url))
                     try:
                         img_file = requests.get(tupian_url, timeout=(30, 30))
                         if img_file.status_code == 200:
@@ -42,23 +40,22 @@ def scrawler(issue_start,issue_end,page):
                             with open(downPath, 'wb') as f:
                                 f.write(img_file.content)
                         else:
-                            print('网站图片无法加载,下载失败')
+                            print('网站图片无法加载，下载失败。链接：%s'%tupian_url)
                     except requests.exceptions.RequestException as e:
                         print(e)
-                        print('网站图片无法加载,下载失败')
-            print('网页' + str(i) + '第%s页图片已下载完成!\n'%page)
+                        print('网站图片无法加载，下载失败。链接：%s'%tupian_url)
         except requests.exceptions.RequestException as e:
             print(e)
-            print('网站加载失败')
+            print('网站加载失败。地址：https://fulibus.net/%s.html/%s' % (str(i), page))
 
 issue_start = input("请输入爬取哪一期开始(例:2019年第1期:输入2019001)\n")
 while issue_start.isdigit() == False or issue_start=='':
     print('请好好输!\n')
     issue_start = input("请输入爬取哪一期开始(例:2019年第1期:输入2019001)\n")
-issue_end = input("请输入爬取哪一期结束(例:2019年第96期:输入2019096)\n")
+issue_end = input("请输入爬取哪一期结束(例:2019年第118期:输入2019118)\n")
 while issue_end.isdigit() == False or issue_end=='':
     print('请好好输!\n')
-    issue_end = input("请输入爬取哪一期结束(例:2019年第100期:输入2019100)\n")
+    issue_end = input("请输入爬取哪一期结束(例:2019年第118期:输入2019118)\n")
 issue_start=int(issue_start)
 issue_end = int(issue_end) + 1
 
